c4afebd15d9ea9ee90114ec01cc901320a96b618
gauborg <gau.borg@gmail.com>
2020-04-18 16:30:16 -0400

    removed camera_cal_outputs folder

--- advanced_lanelines.py
+++ advanced_lanelines.py
@@ -4,16 +4,8 @@ import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import glob
-
-
-def cal_undistort(img, object_points, image_points):
-    # this function saves undistorted images
-
-    # Use cv2.calibrateCamera() and cv2.undistort()
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(object_points, image_points, img.shape[1::-1], None, None)
-    undistorted_image = cv2.undistort(img, mtx, dist, None, mtx)
-
-    return undistorted_image
+import natsort
+import pickle
 
 
 def calibrate_camera(nx, ny):
@@ -22,150 +14,144 @@ def calibrate_camera(nx, ny):
     # get the list of images
     images = glob.glob('camera_cal/calibration*.jpg')
 
+    # sorting the images based on ids
+    images = natsort.natsorted(images)
+    # images = glob.glob('test_images/test*.jpg')
+
     # creating arrays to store object and image points from all the images
     objpoints = []      # 3D object points in real space
     imgpoints = []      # 2D points in image space
 
     # prepare object points
-    objp = np.zeros((nx*ny, 3), np.float32)
+    objp = np.zeros((ny*nx, 3), np.float32)
     objp[:,:2] = np.mgrid[0:nx,0:ny].T.reshape(-1,2)    # x, y cor-ordinates
 
-
-    i = 1
-    for fname in images:
-
-        # read file
-        img = cv2.imread(fname)
-
-        # convert image to grayscale
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        # find the chessboard corners
-        ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
-        print(ret)
-
-        # If found, we will get image coordinates
-        if ret == True:
-            # add the corners and objectpoints to our lists
-            imgpoints.append(corners)
-            objpoints.append(objp)
-
-            # draw and display the corners
-            img = cv2.drawChessboardCorners(img, (nx, ny), corners, ret)
-
-            # save the drawn chessboard corners
-            mpimg.imsave(("camera_cal_outputs/calibration"+str(i)+"-corners.jpg"), img)
-
-            # calculate and save undistorted image
-            undist = cal_undistort(gray, objpoints, imgpoints)
-            mpimg.imsave(("camera_cal_outputs/calibration"+str(i)+"-undistorted.jpg"), undist)
-        
-        i+=1
-
-    print(len(imgpoints))
-
-
-calibrate_camera(9,6)
-
-
-
-
-
-
-'''
-# arrays to store image and object points
-imgpoints = []
-objpoints = []
-
-# prepare object points like (0,0,0), (1,0,0) ... (7,5,0)
-objp = np.zeros((6*9, 3), np.float32)
-objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)    # x, y cor-ordinates
-
-fname = "camera_cal/calibration3.jpg"
-# read file
-img = cv2.imread(fname)
-
-# convert image to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# find the chessboard corners
-ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
-print(ret)
-
-# iterate through the images
-i = 1
-
-# If found, draw corners
-if ret == True:
-
-    imgpoints.append(corners)
-    objpoints.append(objp)
-
-    # draw and display the corners
-    img = cv2.drawChessboardCorners(img, (9, 6), corners, ret)
-    mpimg.imsave(("camera_cal_outputs/calibration-corners.jpg"), img)
-
-
-print(len(imgpoints))
-
-
-
-
-
-def calibrate_camera(nx, ny):
-    # function to calibrate the camera over different chessboard images and input the x and y corners
-
-    # get the list of images
-    images = glob.glob('camera_cal/calibration*.jpg')
-
-    # creating arrays to store object and image points from all the images
-    objpoints = []      # 3D object points in real space
-    imgpoints = []      # 2D points in image space
-
-    # prepare object points
-    objp = np.zeros((6*9, 3), np.float32)
-    objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)    # x, y cor-ordinates
-
-    # convert image to grayscale
-    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
     # iterate through the images
-    i = 1
     for fname in images:
 
         # read file
         img = cv2.imread(fname)
+
+        # grab the filenames and extensions for saving result files later
+        filename_w_ext = os.path.basename(fname)
+        filename, file_extension = os.path.splitext(filename_w_ext)
 
         # convert image to grayscale
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # find the chessboard corners
         ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
-        print(ret)
+        # print(ret)
 
         # If found, we will get image coordinates
-        if ret == True:
+        if (ret == True):
             # add the corners and objectpoints to our lists
             imgpoints.append(corners)
             objpoints.append(objp)
 
             # draw and display the corners
-            img = cv2.drawChessboardCorners(img, (9, 6), corners, ret)
+            img_corners = cv2.drawChessboardCorners(img, (nx, ny), corners, ret)
+            # cv2.imshow('img',img_corners)
+            # cv2.waitKey(0)
 
             # save the drawn chessboard corners
-            mpimg.imsave(("camera_cal/identified_corners/calibration-corners"+str(i)+".jpg"), img)
+            # mpimg.imsave(('output_images/chessboard_corners/'+filename+'-corners'+file_extension), img_corners)
+            # cv2.imwrite(('chessboard_corners/'+filename+'-corners'+file_extension), img_corners)
 
-            # perform camera calibration, given object points, image points, and the shape of the grayscale image
-            # ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-
-            # plt.imshow(img)
-            # plt.show()
+            # calculate and save undistorted images
+            ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img.shape[1::-1], None, None)
+            undist = cv2.undistort(img, mtx, dist, None, mtx)
+            # cv2.imwrite(('output_images/undistorted_chessboard_corners/'+filename+'-undistorted'+file_extension), undist)
         
-        i+=1
-    # print(len(imgpoints))
+
+    # cv2.destroyAllWindows()
+
+    # sample one image for undistortion demo
+    
+    img = cv2.imread('camera_cal/calibration3.jpg')
+    img_size = (img.shape[1], img.shape[0])
+
+    # camera calibration after giving object points and image points
+
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
+    
+    dst = cv2.undistort(img, mtx, dist, None, mtx)
+    cv2.imwrite('output_images/test_undist.jpg',dst)
+    
+    # Save the camera calibration result for use later on
+    dist_pickle = {}
+    dist_pickle["mtx"] = mtx
+    dist_pickle["dist"] = dist
+    pickle.dump(dist_pickle, open( "pickle/wide_dist_pickle.p", "wb" ) )
+    
+    # Visualize undistortion in one image
+
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
+    ax1.imshow(img)
+    ax1.set_title('Original Image', fontsize=30)
+    ax2.imshow(dst)
+    ax2.set_title('Undistorted Image', fontsize=30)
+    # plt.show()
+    
+    return mtx, dist
 
 
+# ----------------------------------------------------------------------- #
 
-calibrate_camera(9, 6)
+# DISTORTION CORRECTION - GET PERSPECTIVE MATRIX
 
-'''
+def unwarp_corners(mtx,dist):
+
+    img = cv2.imread('camera_cal/calibration3.jpg')
+
+    nx = 9
+    ny = 6
+
+    # Use the OpenCV undistort() function to remove distortion
+    undist = cv2.undistort(img, mtx, dist, None, mtx)
+    # Convert undistorted image to grayscale
+    gray = cv2.cvtColor(undist, cv2.COLOR_BGR2GRAY)
+
+    # Search for corners in the grayscaled image
+    ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
+#     print(ret)
+    if ret == True:
+        # If we found corners, draw them! (just for fun)
+        cv2.drawChessboardCorners(undist, (nx, ny), corners, ret)
+        # Choose offset from image corners to plot detected corners
+        # This should be chosen to present the result at the proper aspect ratio
+        # My choice of 100 pixels is not exact, but close enough for our purpose here
+        offset = 100 # offset for dst points
+        # Grab the image shape
+        img_size = (gray.shape[1], gray.shape[0])
+
+        # For source points I'm grabbing the outer four detected corners
+        src = np.float32([corners[0], corners[nx-1], corners[-1], corners[-nx]])
+        # For destination points, I'm arbitrarily choosing some points to be
+        # a nice fit for displaying our warped result 
+        # again, not exact, but close enough for our purposes
+        dst = np.float32([[offset, offset], [img_size[0]-offset, offset],\
+                          [img_size[0]-offset, img_size[1]-offset],\
+                          [offset, img_size[1]-offset]])
+        # Given src and dst points, calculate the perspective transform matrix
+        M = cv2.getPerspectiveTransform(src, dst)
+        # Warp the image using OpenCV warpPerspective()
+        warped = cv2.warpPerspective(undist, M, img_size)
+        
+            # vizualize the data
+        
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+    # f.tight_layout()
+    ax1.imshow(img)
+    ax1.set_title('Original Image', fontsize=20)
+    ax2.imshow(warped)
+    ax2.set_title('Undistorted and Warped Image', fontsize=20)
+    plt.show()
+    # plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+        
+    return M
+
+
+# function calls
+mtx, dist = calibrate_camera(9,6)
+perspective_M = unwarp_corners(mtx, dist)
